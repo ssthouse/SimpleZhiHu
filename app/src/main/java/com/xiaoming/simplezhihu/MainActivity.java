82ab8d4f053d5fa3e297f900fa84b4fcdb26e77b
ssthouse <ssthouse@163.com>
2016-12-29 20:19:45 +0800

    Add sliding menu, just to show the new drawerLayout

--- app/src/main/java/com/xiaoming/simplezhihu/MainActivity.java
+++ app/src/main/java/com/xiaoming/simplezhihu/MainActivity.java
@@ -1,6 +1,9 @@
 package com.xiaoming.simplezhihu;
 
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 
 import com.xiaoming.simplezhihu.base.BaseActivity;
@@ -12,16 +15,31 @@ public class MainActivity extends BaseActivity {
     @Bind(R.id.id_tb)
     Toolbar mToolbar;
 
+    @Bind(R.id.id_navigation)
+    NavigationView mNavigationView;
+
+    @Bind(R.id.id_drawer_view)
+    DrawerLayout mDrawerLayout;
+
     private MainFragment mMainFragment;
 
     @Override
     public void init() {
         initToolbar();
 
+        initDrawer();
+
         mMainFragment = new MainFragment();
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.id_fragment_container, mMainFragment)
                 .commit();
+    }
+
+    private void initDrawer() {
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.str_drawer_des_open, R.string.str_drawer_des_close);
+        actionBarDrawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
     }
 
     private void initToolbar() {
